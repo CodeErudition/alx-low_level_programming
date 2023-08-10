84be70913909ebc4e7754b7e74be0913f269dafc
CodeErudition <infinity3nity360@gmail.com>
2023-08-10 14:31:07 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for a program that prints a line with puts function.
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+	return (0);
+}
