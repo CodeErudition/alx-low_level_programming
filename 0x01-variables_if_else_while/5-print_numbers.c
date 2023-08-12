0e774c4bd2c74b8b6f4ab94d86a32fc2a1ab0864
CodeErudition <infinity3nity360@gmail.com>
2023-08-11 19:48:18 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point.
+ * Return: 0 (succes)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; 1 < 10 ; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+}
