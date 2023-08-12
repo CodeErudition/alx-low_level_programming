d4766723805afb6e18dbad3223b30979e5ecc158
CodeErudition <infinity3nity360@gmail.com>
2023-08-11 21:03:11 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point of this program.
+ * Return: 0 (succes)
+ */
+int main(void)
+{
+	int i;
+	char k;
+
+	for (i = 0 ; 1 < 10 ; i++)
+		putchar(i + '0');
+	for (k = 'a' ; k <= 'f' ; k++)
+		putchar(k);
+	putchar('\n');
+	return (0);
+}
