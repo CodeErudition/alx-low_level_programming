cc9806a6cec2f471ea789c8f4e20d44cce6ec166
CodeErudition <infinity3nity360@gmail.com>
2023-08-16 20:04:21 -0700

    a function that prints the 9 times table, starting with 0,

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - adds two integers and returns the result
+ * @a: int
+ * @b: int
+ * Return: sum of a + b
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+
+	return (sum);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -24,7 +24,7 @@ void times_table(void)
 				_putchar(c + '0');
 			} else if (c >= 10)
 			{
-				_putchar(';');
+				_putchar(',');
 				_putchar(' ');
 				_putchar((c / 10) + '0');
 				_putchar((c % 10) + '0');
