101948da19dad008db54e2de39929e6ea48e82f6
CodeErudition <infinity3nity360@gmail.com>
2023-08-18 21:26:50 -0700

    a function that draws a diagonal line on the terminal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#iaa34a3d6efed11e8e813aa34a3d6efed11e8e813nclude "main.h"
+
+/**
+ * print_diagonal - Prints a diagonal line of backslashes.
+ * @n: The number of lines in the diagonal
+ *  Return: void.
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < i; j++)
+			_putchar(' ');
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
