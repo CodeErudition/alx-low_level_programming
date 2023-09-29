e743fe8786ae35f3ec821a3820cbe7e86805caae
CodeErudition <infinity3nity360@gmail.com>
2023-09-28 20:28:58 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,27 +1,40 @@
 #include "main.h"
 
+int is_prime(int n, int denominator);
 /**
- * is_prime_number - A function that checks if a given integer
- * is a prime number.
+ * is_prime - A helper function to check if a number is prime.
  * @n: The integer to be checked for primality.
+ * @denominator: The current divisor being tested.
  *
  * Return: 1 if 'n' is a prime number, 0 if it's not.
  */
-
-int is_prime_number(int n)
+int is_prime(int n, int denominator)
 {
-	int count;
-
 	if (n <= 1)
 	{
 		return (0);
 	}
-	for (count = 2; count * count <= n; count++)
+
+	if (denominator == 1)
 	{
-		if (n % count == 0)
-		{
-			return (0);
-		}
+		return (1);
 	}
-	return (1);
+
+	if (n % denominator == 0)
+	{
+		return (0);
+	}
+	return (is_prime(n, denominator - 1));
+}
+
+/**
+ * is_prime_number - A function that checks if a given integer is a
+ * prime number.
+ * @n: The integer to be checked for primality.
+ *
+ * Return: 1 if 'n' is a prime number, 0 if it's not.
+ */
+int is_prime_number(int n)
+{
+	return (is_prime(n, n - 1));
 }
