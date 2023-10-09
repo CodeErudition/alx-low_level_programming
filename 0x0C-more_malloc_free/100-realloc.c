2771373d10d5fad030390122a56e1b35f67cc29e
CodeErudition <infinity3nity360@gmail.com>
2023-10-08 19:02:03 -0700

    100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -19,14 +19,18 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (ptr == NULL)
 	{
 		_realloc_ptr = malloc(new_size);
+		if (_realloc_ptr == NULL)
+		{
+			return (NULL);
+		}
 		return (_realloc_ptr);
 	}
-	else if (new_size == 0)
+	if (new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	else if (new_size == old_size)
+	if (new_size == old_size)
 	{
 		return (ptr);
 	}
@@ -35,13 +39,13 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		return (NULL);
 	}
-	else if (new_size > old_size)
+	if (new_size > old_size)
 	{
 		new_size = old_size;
 	}
 	for (i = 0; i < new_size; i++)
 	{
-		_realloc_ptr[i] = ptr[i];
+		((char *)_realloc_ptr)[i] = ((char *)ptr)[i];
 	}
 	free(ptr);
 	return (_realloc_ptr);
