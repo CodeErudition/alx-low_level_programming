a19ce3f789a25e3e3569b5e99985e4ea1ff21b55
CodeErudition <infinity3nity360@gmail.com>
2023-10-09 04:51:30 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -5,7 +5,7 @@
  * is_digit - A function that checks if a string is a valid
  * positive integer.
  *
- * @_str: A pointer to string to check.
+ * @_str: A pointer to the string to check.
  *
  * Return: 1 if the string is a valid positive integer, 0 otherwise.
  */
@@ -24,37 +24,48 @@ int is_digit(char *_str)
 		}
 		_str++;
 	}
+
 	return (1);
 }
 
 /**
- * main - Entry point ofa program that multiplies two positive numbers.
+ * main - Entry point of a program that multiplies two positive numbers.
  *
  * @argc: The number of command arguments.
  * @argv: An array of command strings.
  *
  * Return: 0 on success, else 98.
  */
+
 int main(int argc, char *argv[])
 {
 	char *_first_num, *_second_num;
 	unsigned long int _n1, _n2, _product;
 
-	if (argc != 3)
+	if (argc < 3 || argc > 3)
 	{
 		printf("Error\n");
 		return (98);
 	}
 	_first_num = argv[1];
 	_second_num = argv[2];
-
 	if (!is_digit(_first_num) || !is_digit(_second_num))
 	{
 		printf("Error\n");
 		return (98);
 	}
-	_n1 = strtoul(_first_num, NULL, 10);
-	_n2 = strtoul(_second_num, NULL, 10);
+	_n1 = 0;
+	while (*_first_num)
+	{
+		_n1 = _n1 * 10 + (*_first_num - '0');
+		_first_num++;
+	}
+	_n2 = 0;
+	while (*_second_num)
+	{
+		_n2 = _n2 * 10 + (*_second_num - '0');
+		_second_num++;
+	}
 	_product = _n1 * _n2;
 	printf("%lu\n", _product);
 	return (0);
