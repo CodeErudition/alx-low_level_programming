cbbb18728cabb7113f46ac2ad0d4a422c971cf88
CodeErudition <infinity3nity360@gmail.com>
2023-10-19 21:17:16 -0700

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+
+/**
+ * first_print - A function that prints string first,
+ *
+ * Return: void.
+ */
+
+void first_print(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
