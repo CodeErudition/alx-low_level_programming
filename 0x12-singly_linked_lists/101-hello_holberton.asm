c2c7dde0c6559f54d00a68e1d11e2a3954d2aac4
CodeErudition <infinity3nity360@gmail.com>
2023-10-19 21:57:27 -0700

    fix

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,16 +1,20 @@
 section .data
-	hello db "Hello, Holberton",10
-	format db "%s",0
+    hello db "Hello, Holberton",10  ; The message to be printed, 10 is the newline character
+    hello_len equ $ - hello
 
 section .text
-	global main
-	extern printf
+    global main
 
 main:
-	push rbp
-	mov rdi, format
-	mov rsi, hello
-	call printf
-	pop rbp
-	ret
+    ; Open stdout (file descriptor 1)
+    mov rax, 1                  ; syscall number for sys_write
+    mov rdi, 1                  ; file descriptor 1 (stdout)
+    mov rsi, hello              ; pointer to the message
+    mov rdx, hello_len          ; message length
+    syscall
+
+    ; Exit the program
+    mov rax, 60                 ; syscall number for sys_exit
+    xor rdi, rdi                ; exit status 0
+    syscall
 
