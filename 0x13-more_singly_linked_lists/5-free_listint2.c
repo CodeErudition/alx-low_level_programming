21324371d3146b6f038876c57fccae9578956250
CodeErudition <infinity3nity360@gmail.com>
2023-10-25 01:28:26 -0700

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -3,22 +3,25 @@
 /**
  * free_listint2 - A function that frees a listint_t list.
  *
- * @head: A pointer to a pointer.
+ * @head: A pointer to a pointer to the head of the list.
  *
  * Return: Void.
  */
 
 void free_listint2(listint_t **head)
 {
-	listint_t *current_node;
-	listint_t *next_node;
+	listint_t *current_node, *node_buff;
 
+	if (head == NULL)
+	{
+		return;
+	}
 	current_node = *head;
 	while (current_node != NULL)
 	{
-		next_node = current_node->next;
+		node_buff = current_node->next;
 		free(current_node);
-		current_node = next_node;
+		current_node = node_buff;
 	}
 	*head = NULL;
 }
