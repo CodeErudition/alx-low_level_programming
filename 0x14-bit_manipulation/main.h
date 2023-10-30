eca142157fce1bcae57514df2f1a6493817e46a4
CodeErudition <infinity3nity360@gmail.com>
2023-10-30 03:26:13 -0700

    2-get_bit.c

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 
 typedef unsigned int Uint;
-
+typedef unsigned long int Ulint;
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
