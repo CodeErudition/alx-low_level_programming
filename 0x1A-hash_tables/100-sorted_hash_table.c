0e113f57cbb23c8dc0acad00faadb0c239fa6935
CodeErudition <infinity3nity360@gmail.com>
2024-07-02 02:11:09 -0700

    100-sorted_hash_table.c

--- 0x1A-hash_tables/100-sorted_hash_table.c
+++ 0x1A-hash_tables/100-sorted_hash_table.c
@@ -127,7 +127,7 @@ char *shash_table_get(const shash_table_t *ht, const char *key)
 
 	if (ht == NULL || key == NULL || *key == '\0')
 		return (NULL);
-	
+
 	index = key_index((const unsigned char *)key, ht->size);
 	SHnode = ht->array[index];
 
